fbd233474499b5a014b31c2eb6a2d2e04402b396
Alex Pereyra <Lexthus@gmail.com>
2023-04-11 23:40:15 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var formProceso = $("#form-proceso");
   var totalSteps = $("fieldset").length;
   var currentStep = 1;
@@ -8,14 +8,14 @@ $(document).ready(function() {
   // Ocultar todos los fieldsets menos el primero
   $("fieldset").not(":first").hide();
 
-  $(".next").click(function() {
+  $(".next").click(function () {
     var $activeFieldset = $(this).closest("fieldset");
     $activeFieldset.hide();
     $activeFieldset.next().show();
     setProgressBar(++currentStep);
   });
 
-  $(".prev").click(function() {
+  $(".prev").click(function () {
     var $activeFieldset = $(this).closest("fieldset");
     $activeFieldset.hide();
     $activeFieldset.prev().show();
@@ -28,10 +28,49 @@ $(document).ready(function() {
     $(".progress-bar").css("width", percent + "%");
   }
 
+  $("#form-proceso").submit(function (event) {
+    event.preventDefault();
+
+    // Obtener los datos del formulario
+    var nombre = $("input[name='nombre']").val();
+    var correo = $("input[name='correo']").val();
+    var direccion = $("input[name='direccion']").val();
+    var producto = $("input[name='producto']:checked").val();
+    var cantidad = $("input[name='cantidad']:checked")
+      .map(function () {
+        return $(this).val();
+      })
+      .get();
+    var instructions = $("textarea[name='instructions']").val();
+
+    // Mostrar resumen del pedido
+    $("#pedido-resumen").html("<h2>Resumen de tu pedido</h2>");
+    $("#pedido-resumen").append("<p><strong>Nombre:</strong> " + nombre + "</p>");
+    $("#pedido-resumen").append("<p><strong>Correo:</strong> " + correo + "</p>");
+    $("#pedido-resumen").append("<p><strong>Dirección:</strong> " + direccion + "</p>");
+    $("#pedido-resumen").append("<p><strong>Producto:</strong> " + producto + "</p>");
+    $("#pedido-resumen").append("<p><strong>Cantidad:</strong> " + cantidad + "</p>");
+
+    // Enviar formulario
+    $.ajax({
+      url: "https://jsonplaceholder.typicode.com/posts",
+      method: "POST",
+      data: $(this).serialize(),
+      success: function (data) {
+        console.log(data);
+        alert("Pedido enviado con éxito!");
+        $("#pedido-resumen").show(); // Mostrar el resumen del pedido después de la alerta
+      },
+      error: function () {
+        alert("Error al enviar el pedido");
+      }
+    });
+  });
+
   $.ajax({
     url: "https://jsonplaceholder.typicode.com/posts",
     method: "GET",
-    success: function(data) {
+    success: function (data) {
       console.log(data);
       $("#resultados").html("");
       for (var i = 0; i < data.length; i++) {
@@ -43,14 +82,14 @@ $(document).ready(function() {
         $("#resultados").append(html);
       }
     },
-    error: function() {
+    error: function () {
       alert("Error al consumir la API");
     }
   });
 
-  $('a[href^="#"]').on('click', function(event) {
+  $('a[href^="#"]').on('click', function (event) {
     var target = $(this.getAttribute('href'));
-    if( target.length ) {
+    if (target.length) {
       event.preventDefault();
       $('html, body').stop().animate({
         scrollTop: target.offset().top
